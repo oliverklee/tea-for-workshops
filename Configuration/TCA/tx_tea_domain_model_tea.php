d3f7c46acb959bc678f6120b5cdeb19bceb530e9
Oliver Klee <typo3-coding@oliverklee.de>
2021-08-26 12:13:47 +0200

    Add `Tea.internalNotes` and break XLIFF, PHP < 7.4, and the sniffer

--- Configuration/TCA/tx_tea_domain_model_tea.php
+++ Configuration/TCA/tx_tea_domain_model_tea.php
@@ -29,7 +29,7 @@ $tca = [
         '1' => [
             'showitem' =>
                 '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-                    title, description, image, owner,
+                    title, description, internalNotes, image, owner,
                  --div--;LLL:EXT:tea/Resources/Private/Language/locallang_db.xlf:tx_tea_domain_model_tea.tabs.access,
                     --palette--;;hidden,
                     --palette--;;access,',
@@ -144,6 +144,16 @@ $tca = [
             'config' => [
                 'type' => 'text',
                 'enableRichtext' => true,
+                'rows' => 8,
+                'cols' => 40,
+                'max' => 2000,
+                'eval' => 'trim',
+            ],
+        ],
+        'internalNotes' => [
+            'label' => 'LLL:EXT:tea/Resources/Private/Language/locallang_db.xlf:tx_tea_domain_model_product_tea.internalNotes',
+            'config' => [
+                'type' => 'text',
                 'rows' => 8,
                 'cols' => 40,
                 'max' => 2000,
