a5b3071bd5cdc992565f84ec9166e2fe06762a75
Oliver Klee <typo3-coding@oliverklee.de>
2023-08-14 14:38:36 +0200

    Break the PSR4 verification

--- Tests/Unit/Controller/FrontEndEditorControllerTest.php
+++ Tests/Unit/Controller/FrontEndEditorControllerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace TTN\Tea\Tests\Unit\Controller;
+namespace TTN\Tea\Tests\Controller;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
