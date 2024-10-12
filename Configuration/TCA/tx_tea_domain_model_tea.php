8ed70866d377657ddc1913add2caf6f1c6cc4d99
Oliver Klee <typo3-coding@oliverklee.de>
2024-10-12 14:56:48 +0200

    [TASK] Break CS-Fixer some more

--- Configuration/TCA/tx_tea_domain_model_tea.php
+++ Configuration/TCA/tx_tea_domain_model_tea.php
@@ -210,7 +210,7 @@ return [
                 'size' => 1,
                 'minitems' => 0,
                 'maxitems' => 1,
-                'hideSuggest' => true,
+                'hideSuggest' => true
             ],
         ],
     ],
